35b56cb2a1670f1717758f3a0a7559e4ba6d88d8
samira <s.badamestani@gmail.com>
2018-10-10 16:24:52 +0330

    removed login activity

--- app/src/main/java/com/architecture/clean/di/builder/ActivityBuilder.kt
+++ app/src/main/java/com/architecture/clean/di/builder/ActivityBuilder.kt
@@ -2,7 +2,6 @@ package com.architecture.clean.di.builder
 
 import com.architecture.clean.ui.detail.DetailActivity
 import com.architecture.clean.ui.home.MainActivity
-import com.architecture.clean.ui.login.LoginActivity
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
 
@@ -11,8 +10,6 @@ abstract class ActivityBuilder {
 
     @ContributesAndroidInjector(modules = [MainActivityProviders::class])
     abstract fun bindMainActivity(): MainActivity
-    @ContributesAndroidInjector()
-    abstract fun bindLoginActivity(): LoginActivity
     @ContributesAndroidInjector()
     abstract fun bindDetailActivity(): DetailActivity
 }
